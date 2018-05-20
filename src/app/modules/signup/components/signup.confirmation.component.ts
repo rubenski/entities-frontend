aea1c8e7728e8a4ad9f545d3a10115ffb0f7ef9d
Ruben van Loen <rubenski@gmail.com>
2018-05-20 22:51:59 +0200

    Fixes + started work on email confirmation component

--- src/app/modules/signup/components/signup.confirmation.component.ts
+++ src/app/modules/signup/components/signup.confirmation.component.ts
@@ -2,8 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     moduleId: module.id,
-    templateUrl: './signup.component.html',
-    styleUrls: ['./signup.component.css']
+    templateUrl: './signup.confirmation.component.html'
 })
 export class EmailConfirmationComponent {
 
